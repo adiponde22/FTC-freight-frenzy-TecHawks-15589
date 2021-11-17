e4189280fbd55d510c33f4f3513b7aa4f33cccc1
adiponde22 <adityaponde@gmail.com>
2021-11-17 18:50:30 -0500

    Fourth Upload

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopNew.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopNew.java
@@ -1,32 +1,4 @@
 package org.firstinspires.ftc.teamcode;
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 
 import android.os.Handler;
 
@@ -36,19 +8,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-/**
- * This OpMode uses the common Pushbot hardware class to define the devices on the robot.
- * All device access is managed through the HardwarePushbot class.
- * The code is structured as a LinearOpMode
- *
- * This particular OpMode executes a POV Game style Teleop for a PushBot
- * In this mode the left stick moves the robot FWD and back, the Right stick turns left and right.
- * It raises and lowers the claw using the Gampad Y and A buttons respectively.
- * It also opens and closes the claws slowly using the left and right Bumper buttons.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
+
 
 @TeleOp(name="Ahen Main2", group="Neha")
 // @Disabled
@@ -59,9 +19,7 @@ public class TeleopNew extends LinearOpMode {
     /* Declare OpMode members. */
     HardwareMapCode robot = new HardwareMapCode();   // Use a Redbot's hardware
 
-    double target = 0;
-    double SERVO_SPEED = .1;
-    double yashIsaPanda = 0.008;
+
     private ElapsedTime     runtime = new ElapsedTime();
 
     @Override
@@ -104,67 +62,20 @@ public class TeleopNew extends LinearOpMode {
 
 
 
-
+            //set duck spinner motor to stop when buttons A and B are not being pressed
             if (!gamepad1.a && !gamepad1.b)
             {
                 robot.duckSpin.setPower(0);
             }
+            //spin clockwise
             if (gamepad1.a) {
                 robot.duckSpin.setPower(-0.4);
             }
-
+            //spin counter clockwise
             if (gamepad1.b) {
                 robot.duckSpin.setPower(0.4);
             }
-//
-//            if (gamepad2.a)
-//                robot.autoServo.setPosition(1);
-//            else if (gamepad2.b)
-//                robot.autoServo.setPosition(0);
-//            else if (gamepad2.x)
-//                robot.autoServo.setPosition(0.25);
-//            else if (gamepad2.y)
-//                robot.autoServo.setPosition(0.75);
-//
-//
-////            if (gamepad1.x){
-////                yashIsaPanda = yashIsaPanda+0.001;
-////                telemetry.addData("count","Servo Speed" + yashIsaPanda);
-////                telemetry.update();
-////
-////            }
-////            if (gamepad1.y){
-////                yashIsaPanda = yashIsaPanda-0.001;
-////                telemetry.addData("count", yashIsaPanda);
-////                telemetry.update();
-////            }
-////            if (gamepad1.a){
-////                double halfTurn = 1120/2;
-////                robot.tray.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-////                robot.tray.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-////                int newTarget = robot.tray.getCurrentPosition() - (int)halfTurn;
-////                robot.tray.setTargetPosition(newTarget);
-////                robot.tray.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-////                runtime.reset();
-////                robot.tray.setPower(1);
-////
-////                while (robot.tray.isBusy()){
-////                    telemetry.addData("Status", "Running half turn");
-////                    telemetry.update();
-////                }
-////                robot.tray.setPower(0);
-////                robot.tray.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-////
-////            }
-////            else if (gamepad1.b){
-////                robot.tray.setPower(-0.2);
-////            }
-//            if (!gamepad1.left_bumper && !gamepad1.right_bumper)
-//                robot.tray.setPower(0);
-//            else if (gamepad1.left_bumper)
-//                robot.tray.setPower(0.1);
-//            else if (gamepad1.right_bumper)
-//                robot.tray.setPower(0.1);
+
 
         }
     }
